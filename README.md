cd5a2dbfad6fcb41f94348c272b330c38bd2c54f
moluoxixi <1983531544@qq.com>
2025-05-02 09:40:00 +0800

    feat: nuxt template

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# template
+# 说明
+用于拷贝 Component组织 所有项目
+# 用法
+- 启动项目
+
+```sh
+pnpm installAll
+```
+
+- 新增子模块
+
+```shell
+git submodule add -b <子模块分支> <子模块仓库地址> <子模块目录>
+
+```
+
+- 更新子模块
+
+```shell
+git submodule update --remote
+```
+
+- 删除子模块
+
+```sh
+# 清除git缓存
+git rm --cached <子模块目录>
+rm -rf .git/modules/<子模块目录>
+# 删除对应子模块目录
+rm -rf <子模块目录>
+```
---
+++ installAll.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "$(tput setaf 4)正在更新component项目... $(tput setaf 3)"
+# 切换到主分支
+git checkout main
+# 拉取主分支的最新更改
+git pull
+
+echo "$(tput setaf 4)正在下载依赖... $(tput setaf 3)"
+pnpm install
+
+# 更新子模块到其远程跟踪分支的最新提交（使用 --rebase 可以避免合并提交，保持历史干净）
+# 注意：这里使用了 --rebase，但您也可以根据需要选择 --merge
+echo "$(tput setaf 4)正在更新子模块... $(tput setaf 3)"
+git submodule update --init
+
+echo "$(tput setaf 4)正在下载子模块依赖... $(tput setaf 3)"
+git submodule foreach "git checkout main && git pull origin main && pnpm installAll"
+
+echo "$(tput setaf 2)子模块依赖下载完毕"
+
+echo "$(tput setaf 4)正在同步子模块到.git/config中... $(tput setaf 3)"
+git submodule sync
